2e4e17d620d650af8c47f7bedc1f5dd757f8327a
Gowrav Vishwakarma <gowrav@gowrav-Lenovo-G570.(none)>
2014-01-29 00:00:56 +0530

    titalization for all owner side pages

--- page/owner/users.php
+++ page/owner/users.php
@@ -2,7 +2,8 @@
 class page_owner_users extends page_base_owner {
 	function init(){
 		parent::init();
-		// $this->add('View_Info')->set($this->api->current_website['name']);
+		$this->add( 'H1' )->setHTML( "User Management <small>Manage your website / applications registered users</small>" );
+
 		$crud=$this->add('CRUD');
 		$usr=$this->add('Model_Users');
 		$usr->addCondition('epan_id',$this->api->current_website->id);
